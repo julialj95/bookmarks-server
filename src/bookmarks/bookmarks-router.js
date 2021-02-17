af784fb6917ab5d75b72a84fd7580a5d448ee5b3
Julia Johnson <julialj95@gmail.com>
2021-02-16 21:10:37 -0600

    fixed logger error

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,44 +1,54 @@
 const express = require("express");
-const { v4: uuid } = require(uuid);
-const { logger } = require("../logger.js");
+const { v4: uuid } = require("uuid");
+const logger = require("../logger.js");
 const { bookmarks } = require("../store.js");
 
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 
 bookmarksRouter
-  // returns a list of bookmarks
   .route("/bookmarks")
   .get((req, res) => {
     res.json(bookmarks);
   })
-  //assigns a new id and posts bookmark to the list
   .post(bodyParser, (req, res) => {
-    const { bookmark } = req.body;
+    const { title, url, desc, rating } = req.body;
 
-    if (!bookmark) {
-      logger.error("Bookmark is required");
-      res.status(400).send("Please enter valid bookmark data.");
+    if (!title) {
+      logger.error("Title is required");
+      res.status(400).send("Please enter valid bookmark title.");
+    }
+    if (!url) {
+      logger.error("Url is required");
+      res.status(400).send("Please enter a valid bookmark url.");
+    }
+    if (!desc) {
+      logger.error("Description is required");
+      res.status(400).send("Please enter a valid bookmark description");
+    }
+    if (!rating) {
+      logger.error("Rating is required");
+      res.status(400).send("Please enter a valid bookmark rating.");
     }
 
     const id = uuid();
 
-    const bookmark = {
+    const newBookmark = {
+      id,
       title,
       url,
-      description,
+      desc,
       rating,
-      id,
     };
 
-    bookmarks.push(bookmark);
+    bookmarks.push(newBookmark);
 
     logger.info(`Bookmark with id ${id} created.`);
 
     res
       .status(201)
       .location(`http://localhost:8000/bookmarks/${id}`)
-      .json(bookmark);
+      .json(newBookmark);
   });
 
 bookmarksRouter
@@ -65,5 +75,14 @@ bookmarksRouter
       return res.status(404).send("Not found");
     }
 
+    for (let i = 0; i < bookmarks.length; i++) {
+      if (bookmarks[i].id === id) {
+        bookmarks.splice(i, 1);
+      }
+    }
+
     logger.info(`Card with id ${id} deleted.`);
+    res.status(204).end();
   });
+
+module.exports = bookmarksRouter;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const app = require("./app");
+const app = require("./app.js");
 const { PORT } = require("./config");
 
 app.listen(PORT, () => {
